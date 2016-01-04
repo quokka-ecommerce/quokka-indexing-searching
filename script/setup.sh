42d358e7b0a324aceb6a6345236fa82c0fd77efc
wcatp19891104@gmail.com <Hurtinheart1989>
2016-01-03 23:00:08 -0800

    fix bug of setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -9,10 +9,11 @@ solr_conf_location=${base_dir}/${solr_name}/server/solr/quokka-search/conf/
 mkdir quokka_solr
 mkdir ${base_dir}/tmp
 
-
-#curl -O http://apache.mirrors.lucidnetworks.net/lucene/solr/5.4.0/solr-5.4.0.zip ./tmp
+cd ${base_dir}/tmp
+curl -O http://apache.mirrors.lucidnetworks.net/lucene/solr/5.4.0/solr-5.4.0.zip
+cd ${current_dir}
 pwd
-cp ~/Documents/asana/solr-5.4.0.zip ${base_dir}/tmp
+#cp ~/Documents/asana/solr-5.4.0.zip ${base_dir}/tmp
 unzip ${base_dir}/tmp/solr-5.4.0.zip -d ${base_dir}/
 
 # start solr
@@ -23,8 +24,11 @@ ${base_dir}/${solr_name}/bin/solr create -c quokka-search -p 8983
 
 # copy config and lib
 cp -R ${current_dir}/conf/solr_conf/* ${base_dir}/${solr_name}/server/solr/quokka-search/conf/
-cp -R ${current_dir}/lib/* ${base_dir}/${solr_name}/server/ex_lib/
-cp -R ${current_dir}/lib/* ${base_dir}/quokka_solr/${solr_name}/server/solr-webapp/webapp/WEB-INF/ex_lib/
+cp -R ${current_dir}/ex_lib/* ${base_dir}/${solr_name}/server/lib/
+cp -R ${current_dir}/ex_lib/* ${base_dir}/${solr_name}/server/solr-webapp/webapp/WEB-INF/lib/
+
+# restart solr
+${base_dir}/${solr_name}/bin/solr restart
 
 
 
